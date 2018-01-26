fc1aee889bf8960fb0e3107c9c395c857127ff3d
noevents <anonananasik@gmail.com>
2018-01-26 13:58:12 +0300

    init

---
+++ index.js
@@ -0,0 +1,13 @@
+const static = require('node-static');
+
+const port = process.env.PORT || 8080
+const file = new static.Server({headers: {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET',
+  'Access-Control-Allow-Headers': 'Content-Type'
+}});
+
+require('http').createServer(function (request, response) {
+  file.serve(request, response);
+}).listen(port);
+console.log(`Server running on port ${port}`);
