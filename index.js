39b2de0d96c236289d8f49557750a3c53b7fa097
noevents <anonananasik@gmail.com>
2018-01-26 14:08:05 +0300

    upd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const static = require('node-static');
 
 const port = process.env.PORT || 8080
 const file = new static.Server({headers: {
-  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': 'https://noevents.github.io',
   'Access-Control-Allow-Methods': 'GET',
   'Access-Control-Allow-Headers': 'Content-Type'
 }});
